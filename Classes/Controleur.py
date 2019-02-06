d529a11ad57d1bf71cfe596a36f11503ba049acd
Lucas Basset <lbasset99@gmail.com>
2019-02-06 16:44:21 +0100

    HUD

--- Classes/Controleur.py
+++ Classes/Controleur.py
@@ -41,6 +41,9 @@ lvl_20 = pygame.image.load("images/2.png").convert_alpha()
 lvl_2 = pygame.transform.scale(lvl_20, (300, 100))
 lvl_30 = pygame.image.load("images/3.png").convert_alpha()
 lvl_3 = pygame.transform.scale(lvl_30, (300, 100))
+empty0 = pygame.image.load("images/empty.png").convert_alpha()
+empty = pygame.transform.scale(empty0, (400, 100))
+
 
 
 #==============================================================================#
@@ -258,6 +261,10 @@ def timeout():
 def GameLoop():
     global level_en_cours_numero
 
+    if event.type == QUIT:
+        pygame.quit()
+        sys.exit()
+
     GameRun = True
     GameOver = False
     score = 0
@@ -289,86 +296,90 @@ def GameLoop():
 
     while GameRun:
 ################################################################################
-            global INT
-            global TIMEAFFICH
-            INT = INT+1
+        global INT
+        global TIMEAFFICH
+        INT = INT+1
 
-            if INT == 59 :
-                INT = 0
-                TIMEAFFICH = TIMEAFFICH-1
+        if INT == 59 :
+            INT = 0
+            TIMEAFFICH = TIMEAFFICH-1
 ################################################################################
 
-            while GameOver == True:
-                time.sleep(1)
-                pygame.mixer.music.stop()
-                Menu_gameover()
+        while GameOver == True:
+            time.sleep(1)
+            pygame.mixer.music.stop()
+            Menu_gameover()
 
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    pygame.quit()
-                    sys.exit()
-            if event.type == KEYDOWN:
-                if event.key == K_LEFT:
-                    vitesseX = -5
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT:
+                vitesseX = -5
 
-                if event.key == K_RIGHT:
-                    vitesseX = 5
+            if event.key == K_RIGHT:
+                vitesseX = 5
 
-                if event.key == K_SPACE and player.auSol == True:
-                    spacePressed = True
-                    vitesseY = -1
+            if event.key == K_SPACE and player.auSol == True:
+                spacePressed = True
+                vitesseY = -1
 
-            if event.type == KEYUP:
-                if event.key == K_LEFT:
-                    vitesseX = 0
-                if event.key == K_RIGHT:
-                    vitesseX = 0
-                if event.key == K_SPACE and spacePressed == True:
-                    player.saut(-10)
-                    spacePressed = False
-                    vitesseY = 1
+        if event.type == KEYUP:
+            if event.key == K_LEFT:
+                vitesseX = 0
+            if event.key == K_RIGHT:
+                vitesseX = 0
+            if event.key == K_SPACE and spacePressed == True:
+                player.saut(-10)
+                spacePressed = False
+                vitesseY = 1
 
-            player.update(vitesseX,vitesseY)
-
-
-            #collision_player_missile_mask =  pygame.sprite.spritecollide(player,level_en_cours.pro_list,False,pygame.sprite.collide_mask)
-            #collision_player_missile = pygame.sprite.spritecollide(player,level_en_cours.pro_list,False)
-            collision_player_fin = pygame.sprite.spritecollide(player,level_en_cours.portal,False)
-            collision_portable = pygame.sprite.spritecollide(player,level_en_cours.portables_list,True)
-
-            if collision_portable:
-                score += 1
-
-            global TIMEFINI
-
-            if TIMEFINI:
-                time.sleep(1)
-                GameOver = True
-                pygame.mixer.music.stop()
-                TIMEFINI=False
-                Menu_Victoire()
-
-            elif collision_player_fin:
-                time.sleep(1)
-                pygame.mixer.music.stop()
-                Menu_Victoire()
-
-            else :
-                level_en_cours.update()
-                level_en_cours.draw(Display)
-                sprite_bouge.draw(Display)
-
-                font = pygame.font.SysFont('verdanaprocondblack', 50)
-                timerScreen = font.render("Timer : "+str(TIMEAFFICH)+" s",1,(255,255,255))
-                Display.blit(timerScreen, (20, 20))
-                #attention pas synchro avec timer
-
-                scoreScreen = font.render("Score : " + str(score) + " pts",1,(255,255,255))
-                Display.blit(scoreScreen, (500, 20))
-
-                # rafraichi l'ecran
-                pygame.display.update()
+        player.update(vitesseX,vitesseY)
 
 
-                # gere les FPS
-                clock.tick(FPS)
+        #collision_player_missile_mask =  pygame.sprite.spritecollide(player,level_en_cours.pro_list,False,pygame.sprite.collide_mask)
+        #collision_player_missile = pygame.sprite.spritecollide(player,level_en_cours.pro_list,False)
+        collision_player_fin = pygame.sprite.spritecollide(player,level_en_cours.portal,False)
+        collision_portable = pygame.sprite.spritecollide(player,level_en_cours.portables_list,True)
+
+        if collision_portable:
+            score += 1
+
+        global TIMEFINI
+
+        if TIMEFINI:
+            time.sleep(1)
+            GameOver = True
+            pygame.mixer.music.stop()
+            TIMEFINI=False
+            Menu_Victoire()
+
+        elif collision_player_fin:
+            time.sleep(1)
+            pygame.mixer.music.stop()
+            Menu_Victoire()
+
+        else :
+            level_en_cours.update()
+            level_en_cours.draw(Display)
+            sprite_bouge.draw(Display)
+
+            Display.blit(empty,(20,10))
+            Display.blit(empty,(500,10))
+            Display.blit(pause,(850,10))
+
+            font = pygame.font.SysFont('verdanaprocondblack', 50)
+            timerScreen = font.render("Timer : "+str(TIMEAFFICH)+" s",1,(255,255,255))
+            Display.blit(timerScreen, (50, 20))
+            #attention pas synchro avec timer
+
+            scoreScreen = font.render("Score : " + str(score) + " pts",1,(255,255,255))
+            Display.blit(scoreScreen, (530, 20))
+
+            # rafraichi l'ecran
+            pygame.display.flip()
+            pygame.display.update()
+
+            # gere les FPS
+            clock.tick(FPS)
